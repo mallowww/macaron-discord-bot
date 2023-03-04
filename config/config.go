461ea2eb64f9f1def54e71565cb28d1f260c7790
mell <chanon4817@gmail.com>
2023-03-04 19:41:15 +0700

    feat: config dicord bot

--- config/config.go
+++ config/config.go
@@ -1,6 +1,39 @@
 package config
 
-var (
-	Token string
-	BotPrefix string
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 )
+
+var (
+	Token     string
+	BotPrefix string
+
+	config *configStruct
+)
+
+type configStruct struct {
+	Token     string `json:"Token"`
+	BotPrefix string `json:"BotPrefix"`
+}
+
+func ReadConfig() error {
+	fmt.Println("reading a config file")
+	file, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	fmt.Println(string(file))
+
+	err = json.Unmarshal(file, config)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	
+	Token = config.Token
+	BotPrefix = config.BotPrefix
+	return nil
+}
